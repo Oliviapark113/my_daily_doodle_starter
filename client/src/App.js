5b89f2a3e76a9d9a45e548078114bba63d177c3e
Olivia Park <oliviaypark113@gmail.com>
2021-04-06 17:05:52 -0400

    updated routerApp

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Draw from './pages/draw'
+import Home from './pages/home'
+import Nav from './components/nav'
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/draw">
+          <Draw />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 
 export default App;
---
+++ client/src/pages/draw.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Draw = () =>(
+    <div>HOME</div>
+)
+
+export default Draw
