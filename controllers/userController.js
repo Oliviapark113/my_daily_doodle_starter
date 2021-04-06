e9b71fe01cf52aed3864f0748388e1d20ff65db5
Olivia Park <oliviaypark113@gmail.com>
2021-04-06 15:42:30 -0400

    updated useController

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const db = require('../models')
+
+module.exports = {
+  getUser: function(req, res) {
+    const {id } = req.params
+    console.log(id)
+    res.send(id)
+   
+  },
+  updateUser: function(req, res) {
+ 
+  }
+}
---
+++ routes/api-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+
+const drawingController = require('../controllers/drawingController')
+
+const userController = require('../controllers/userController')
+
+router.route("/api/drawings")
+.get(drawingController.getDrawings)
+.post(drawingController.createDrawing)
+
+
+router.route('api/drawings/:id')
+.put(drawingController.updateDrawing)
+.delete(drawingController.deleteDrawing)
+
+router.route('/api/users/:id')
+.get(userController.getUser)
+.put(userController.updateUser)
+
+module.exports = router
